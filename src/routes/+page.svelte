4921d7d7ab466272e7cf0a43059d667acebb8136
ulriksun <ulriksun@gmail.com>
2024-01-15 13:46:43 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+	
+<script lang="ts">
+    // import { Label } from "$lib/components/ui/label";
+    import { Switch } from "$lib/components/ui/switch";
+    let on = false;
+</script>
+  
+  <div class="flex items-center space-x-2">
+    <Switch id="airplane-mode" bind:checked={on}/>
+    <!-- <Label for="airplane-mode">Airplane Mode</Label> -->
+    {#if on}
+      <p>
+        ON
+      </p>
+    {:else}
+      <p>
+        OFF
+      </p>
+    {/if}
+  </div>
+  
+  
