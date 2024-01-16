655f18c4cbd45dd15daaa3234cf4c4f7ed34026c
ulriksun <ulriksun@gmail.com>
2024-01-16 16:08:18 +0100

    svelte5 shadcn question module radio buttons not using shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,14 +5,21 @@
     import { Input } from '$components/ui/input';
     import { MCSelectOne } from '../lib/components/MCSelectOne';
     let width = 400;
-    let height = 300;
+    let height = 250;
     import {data} from "$components/data"
-    let value = "";
+    let value = $state("");
+    let correctAnswer = data[0].data.alternatives.filter( (i) => {
+      return i.correct
+    })[0].answer;
+    let answerIsCorrect = $derived(value === correctAnswer);
 </script>
 
 <div style="width: {width}px; height: {height}px; border:solid black">
   <MCSelectOne question={data[0]} bind:value />
 </div>
-<p>Kandidaten valgte {value}</p>
+<p>Kandidaten valgte {value}, som er {answerIsCorrect ? "riktig" : "galt"}</p>
+<p>
+  Riktig svar er {correctAnswer}
+</p>
   
   
